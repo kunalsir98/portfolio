4636160e8f5777a5da9b2c55e1b580f246f00bd1
kunalchopde <kunalchopde98@gmail.com>
2024-09-24 17:25:34 +0530

    done:

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html', title="Home")
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html', title="Projects")
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', title="Contact")
+
+if __name__ == "__main__":
+    app.run(debug=True)
